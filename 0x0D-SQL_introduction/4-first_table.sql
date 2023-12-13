8b06cfdbb55b17def12c57d8e6783301c27c7a10
xWESOOOx <wesalkishky@outlook.com>
2023-12-13 03:31:50 +0200

    updated

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
-led first_table with values id and name in my MySQL server.
+-- Creates a table called first_table with values id and name in my MySQL server.
 CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
